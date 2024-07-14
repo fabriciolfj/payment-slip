2e9675610337b5da5cfb70128134e17f3368b4e1
spark <fabricio.jacob@outlook.com>
2024-07-14 18:06:23 -0300

    init project

---
+++ src/test/java/com/github/paymentslip/PaymentslipApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.paymentslip;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentslipApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
