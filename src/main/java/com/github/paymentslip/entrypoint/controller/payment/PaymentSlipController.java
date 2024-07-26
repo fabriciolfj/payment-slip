c1af201a765abb55c297a92180efe14d13284920
spark <fabricio.jacob@outlook.com>
2024-07-25 23:28:43 -0300

    init project

--- src/main/java/com/github/paymentslip/entrypoint/controller/payment/PaymentSlipController.java
+++ src/main/java/com/github/paymentslip/entrypoint/controller/payment/PaymentSlipController.java
@@ -2,8 +2,11 @@ package com.github.paymentslip.entrypoint.controller.payment;
 
 import io.smallrye.mutiny.Uni;
 import jakarta.validation.Valid;
+import jakarta.ws.rs.Consumes;
 import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 import lombok.extern.slf4j.Slf4j;
 
@@ -12,6 +15,8 @@ import lombok.extern.slf4j.Slf4j;
 public class PaymentSlipController {
 
     @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
     public Uni<Response> createPayment(@Valid final PaymentRequest paymentRequest) {
         return Uni.createFrom().item(paymentRequest)
                 .invoke(value -> log.info("Payment request: {}", value))
